caa9fe7b81eba0f74249ede02e671253732b5067
diderot-sielinou <diderotsielinou@gmail.com>
2025-04-23 12:47:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from'express';
+import path,{dirname}  from'path';
+import { fileURLToPath } from 'node:url';
+import cookieParser from'cookie-parser';
+import logger from'morgan';
+
+import  indexRouter from'./routes/index.js';
+import usersRouter from'./routes/users.js';
+
+const app = express();
+const __filname  = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filname)
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
