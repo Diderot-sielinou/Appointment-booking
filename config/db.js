9e8be83041007f229d9068dfb41346f9710f8e85
diderot-sielinou <diderotsielinou@gmail.com>
2025-04-24 11:17:39 +0100

    login service provider

--- config/db.js
+++ config/db.js
@@ -136,26 +136,26 @@ const initialzeDbSchema = async () => {
     logger.debug("update_updated_at_column function ensured.");
 
     //-- tigger for service_providers
-    await client.query(`CREATE TRIGGER trg_service_providers_updated_at
-                       BEFORE UPDATE ON service_providers
-                       FOR EACH ROW
-                       EXECUTE FUNCTION update_modified_at();`);
-  logger.debug("service_providers update_at Trigger is checked and created")
+  //   await client.query(`CREATE TRIGGER trg_service_providers_updated_at
+  //                      BEFORE UPDATE ON service_providers
+  //                      FOR EACH ROW
+  //                      EXECUTE FUNCTION update_modified_at();`);
+  // logger.debug("service_providers update_at Trigger is checked and created")
     //-- tigger for clients
-    await client.query(`CREATE TRIGGER trg_clients_updated_at
-                        BEFORE UPDATE ON clients
-                        FOR EACH ROW
-                        EXECUTE FUNCTION update_modified_at();`);
+    // await client.query(`CREATE TRIGGER trg_clients_updated_at
+    //                     BEFORE UPDATE ON clients
+    //                     FOR EACH ROW
+    //                     EXECUTE FUNCTION update_modified_at();`);
     //--tigger for time_slots
-    await client.query(`CREATE TRIGGER trg_time_slots_updated_at
-                        BEFORE UPDATE ON time_slots
-                        FOR EACH ROW
-                        EXECUTE FUNCTION update_modified_at();`);
+    // await client.query(`CREATE TRIGGER trg_time_slots_updated_at
+    //                     BEFORE UPDATE ON time_slots
+    //                     FOR EACH ROW
+    //                     EXECUTE FUNCTION update_modified_at();`);
     //-- tigeger for appointment
-    await client.query(`CREATE TRIGGER trg_appointment_updated_at
-                        BEFORE UPDATE ON appointment
-                        FOR EACH ROW
-                        EXECUTE FUNCTION update_modified_at();`);
+    // await client.query(`CREATE TRIGGER trg_appointment_updated_at
+    //                     BEFORE UPDATE ON appointment
+    //                     FOR EACH ROW
+    //                     EXECUTE FUNCTION update_modified_at();`);
     
   } catch (error) {
     logger.error(`Error while initializing the schema`, error);
--- routes/auth-service-provider.js
+++ routes/auth-service-provider.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import registerServiceProviderHandle from '../controller/register-serviceProvider-controller.js';
+import { loginServiceProviderHandle } from '../controller/login-service_provider.js';
 
 const  router = express.Router(); 
 
 router.post('/register',registerServiceProviderHandle)
+router.post('/login',loginServiceProviderHandle)
 
 export default router
